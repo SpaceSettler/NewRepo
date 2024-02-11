6a277a1091e00d1eec2888e1a3063d5f0b958968
SpaceSettler <j.devon.richardson@gmail.com>
2024-02-11 02:03:44 -0700

    file error fix

---
+++ Wk 4/scripts/w04-task.js
@@ -0,0 +1,41 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "J",
+    photo: "C:\Users\jdevo\OneDrive\Documents\School Work\Intro to Javascript\cse121b\Wk 2\images\profile.png",
+    favoriteFoods: ["Pizza", "Coleslaw", "Rice", "Mayonnaise"],
+    hobbbies: ["Golf", "Lounge", "Eat", "Bird Watching"],
+    placesLived: [],
+};
+myProfile.placesLived.push(
+    {
+        place: "35°29'50\"N 138°45'17\"E",
+        length: "[REDACTED]"
+    }
+);
+document.querySelector('#name').textContent = myProfile.name;
+document.querySelector('#photo').src = myProfile.photo;
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
